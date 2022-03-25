5322415f7af76955e212a6a2ee2ff22f4e0add34
beland.dev <>
2022-03-25 21:04:48 +0700

    update list scenes

--- src/api/scenes/index.ts
+++ src/api/scenes/index.ts
@@ -1,10 +1,35 @@
+import { Op } from 'sequelize';
 import database from '../../database';
+import { getParcelIdsFromPointers } from '../../utils/parcel';
 import { buildQuery } from '../../utils/query';
+const { scene: Scene, parcel: Parcel } = database.models;
 
-const Scene = database.models.scene;
 export async function listScenes(ctx) {
+  let includes = [];
+  buildQueryPointer(includes, ctx);
   const query = buildQuery(ctx);
-  const items = await Scene.findAndCountAll(query);
+  query.include = includes;
+  const res = await Scene.findAndCountAll(query);
   ctx.status = 200;
-  ctx.body = items;
+  ctx.body = res;
+}
+
+function getPointers(pointers) {
+  if (!pointers) return [];
+  return Array.isArray(pointers) ? pointers : [pointers];
+}
+
+function buildQueryPointer(includes, ctx) {
+  const pointers = getPointers(ctx.query.pointers);
+  if (pointers.length === 0) {
+    return;
+  }
+  delete ctx.query.pointers;
+  const parcelIds = getParcelIdsFromPointers(pointers);
+  includes.push({
+    model: Parcel,
+    required: true,
+    attributes: [],
+    where: { id: { [Op.in]: parcelIds } },
+  });
 }
