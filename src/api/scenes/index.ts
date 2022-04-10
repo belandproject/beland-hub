e683d831da8b994a6b331c44a9db2199bc338c5c
beland.dev <>
2022-04-10 09:17:43 +0700

    fix api list scenes

--- src/api/scenes/index.ts
+++ src/api/scenes/index.ts
@@ -12,14 +12,7 @@ export async function listScenes(ctx) {
   query.include = includes;
   const res = await Scene.findAndCountAll(query);
   ctx.status = 200;
-  res.rows = res.rows.map(row => {
-    row.pointers = row.parcels.map(parcel => {
-      return getPointerFromID(parcel.id)
-    })
-    delete row.parcels;
-    return row;
-  })
-  ctx.body = res;
+  ctx.body = formatListScenes(res);
 }
 
 function getPointers(pointers) {
@@ -40,4 +33,24 @@ function buildQueryPointer(includes, ctx) {
     attributes: ['id'],
     where: { id: { [Op.in]: parcelIds } },
   });
+}
+
+
+const formatListScenes = (data) => {
+  const rows = [];
+  for (let i = 0 ;i < data.rows.length; i ++) {
+    let row: any= data.rows[i].toJSON();
+    if (row.parcels) {
+      row.pointers = row.parcels.map(parcel => {
+        const xy = getPointerFromID(parcel.id)
+        return `${xy.x},${xy.y}`
+      })
+      delete row.parcels;
+    }
+    rows.push(row)
+  }
+  return {
+    count: data.count,
+    rows,
+  }
 }
