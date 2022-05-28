04fd48dc43061970ad6637cb92228503cce67c2f
belanddev <100743066+belanddev@users.noreply.github.com>
2022-05-28 14:27:26 +0700

    Update index.ts

--- src/api/bids/index.ts
+++ src/api/bids/index.ts
@@ -7,29 +7,37 @@ const User = database.models.user;
 
 async function list(ctx) {
   const nftWhere: any = {};
+  const include = [];
   if (ctx.query.seller) {
     nftWhere.owner = ctx.query.seller;
     delete ctx.query.seller;
+  }
+  
+  if (ctx.query.include && ctx.query.include.includes('nft')) {
+    include.push(
+      [
+        {
+          model: Nft,
+          as: 'nft',
+          where: nftWhere,
+          attributes: ['id', 'name', 'imageUrl'],
+          include: [
+            {
+              model: User,
+              attributes: ['name', 'avatar', 'website'],
+              as: 'creatorInfo',
+            },
+          ],
+        },
+      ]
+    )
   }
 
   const query = buildQuery(ctx);
 
   const nfts = await Bid.findAndCountAll({
     ...query,
-    include: [
-      {
-        model: Nft,
-        as: 'nft',
-        where: nftWhere,
-        include: [
-          {
-            model: User,
-            attributes: ['name', 'avatar', 'website'],
-            as: 'creatorInfo',
-          },
-        ],
-      },
-    ],
+    include,
   });
   ctx.status = 200;
   ctx.body = nfts;
