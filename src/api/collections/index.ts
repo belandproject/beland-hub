dd0ae7d6ec62d4803d196e3d66d591575c79cf95
beland.dev <beland.developer@gmail.com>
2022-06-22 08:35:57 +0700

    fix

--- src/api/collections/index.ts
+++ src/api/collections/index.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-joi-router';
 import { checkAuthMiddleware } from '../../middlewares/auth';
+import { gossipMiddleware } from '../../middlewares/gossip';
 import { handleGet, handleList, handleUpdate } from './collection.handlers';
 import { collectionUpdateValidate } from './collection.validate';
 import { canUpdateColMiddleware, getCollectionMiddleware } from './middleware';
@@ -8,13 +9,15 @@ const collectionRouter = new Router();
 
 collectionRouter.get('/:id', handleGet);
 collectionRouter.get('/', handleList);
-collectionRouter.put(
-  '/:id',
-  checkAuthMiddleware,
-  collectionUpdateValidate,
-  getCollectionMiddleware,
-  canUpdateColMiddleware,
-  handleUpdate
-);
+
+collectionRouter.route([
+  {
+    method: 'put',
+    path: '/:id',
+    validate: collectionUpdateValidate,
+    pre: checkAuthMiddleware,
+    handler: [gossipMiddleware, getCollectionMiddleware, canUpdateColMiddleware, handleUpdate],
+  },
+]);
 
 export default collectionRouter;
